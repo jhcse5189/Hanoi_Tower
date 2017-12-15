425d5e17e5559550f5537aa50b9088b87971ee68
김병준 <qudwns0524@gmail.com>
2017-12-16 06:07:36 +0900

    Create ht.py

---
+++ ht.py
@@ -0,0 +1,14 @@
+#CODED BY BYUNG JUN KIM, 2017.12.16, 6:06 AM
+def ht(n, a, b, c) :
+    if (n == 1) :
+        print(str(a) + " " + str(c));
+    else :
+        ht(n - 1, a, c, b);
+        print(str(a) + " " + str(c));
+        ht(n - 1, b, a, c);
+n = int(input());
+power = 1;
+for i in range(0, n) :
+    power *= 2;
+print(str(power - 1));
+ht(n, 1, 2, 3);
